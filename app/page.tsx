96a4caf48c15734501c60a304f01016d215bfaac
Benjamin <147251418+loomiedave@users.noreply.github.com>
2026-07-07 10:24:34 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { Suspense } from "react";
+import OnboardingContent from "@/components/OnboardingContent";
 
-async function AuthRedirect() {
+async function AuthCheck() {
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (user) redirect("/dashboard");
-  else redirect("/auth/login");
-
-  // eslint-disable-next-line no-unreachable
-  return null;
+  return <OnboardingContent signedIn={!!user} />;
 }
 
 export default function Home() {
@@ -24,7 +20,7 @@ export default function Home() {
         </div>
       }
     >
-      <AuthRedirect />
+      <AuthCheck />
     </Suspense>
   );
 }
