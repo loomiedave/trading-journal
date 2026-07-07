4445fc1ca6208592382c4056133aedaf1f191a80
loomiedave <loomiedave@gmail.com>
2026-07-07 14:25:39 +0000

    pwa intg

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
   allowedDevOrigins: ["192.168.1.96", "192.168.1.70", "192.168.1.68"],
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
